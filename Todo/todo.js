e6568210df22009281993d5bc7846a46c29919ff
srijan0412 <95132945+srijan0412@users.noreply.github.com>
2023-03-24 08:03:44 +0530

    adding js

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,4 +1,69 @@
 // Todo-List Manipulation
+let btn_today = document.getElementById("btn_today");
+let btn_bookmark = document.getElementById("btn_bookmark");
+let btn_home = document.getElementById("btn_home");
+let btn_calendar = document.getElementById("btn_calendar");
+
+let today = document.getElementById("today");
+let bookmark = document.getElementById("bookmark");
+let home = document.getElementById("home");
+let calendar = document.getElementById("calendar");
+
+// display_none <- to make display as none
+
+btn_today.addEventListener('click', ()=>{
+    // .opened <- declare btn as open 
+    btn_today.classList.add('opened');
+    today.classList.remove('display_none');
+
+    // Resetting other buttons
+    btn_bookmark.classList.remove('opened');
+    btn_home.classList.remove('opened');
+    btn_calendar.classList.remove('opened');
+
+    // Display others as none
+    bookmark.classList.add('display_none');
+    home.classList.add('display_none');
+    calendar.classList.add('display_none');
+});
+
+btn_bookmark.addEventListener('click', ()=>{
+    // .opened <- declare btn as open 
+    btn_bookmark.classList.add('opened');
+    bookmark.classList.remove('display_none');
+
+    // Resetting other buttons
+    btn_today.classList.remove('opened');
+    btn_home.classList.remove('opened');
+    btn_calendar.classList.remove('opened');
+
+    // Display others as none
+    today.classList.add('display_none');
+    home.classList.add('display_none');
+    calendar.classList.add('display_none');
+});
+
+btn_home.addEventListener('click', ()=>{
+    // .opened <- declare btn as open 
+    btn_home.classList.add('opened');
+
+    // Resetting other buttons
+    btn_today.classList.remove('opened');
+    btn_bookmark.classList.remove('opened');
+    btn_calendar.classList.remove('opened');
+});
+
+btn_calendar.addEventListener('click', ()=>{
+    // .opened <- declare btn as open 
+    btn_calendar.classList.add('opened');
+
+    // Resetting other buttons
+    btn_today.classList.remove('opened')
+    btn_bookmark.classList.remove('opened')
+    btn_home.classList.remove('opened')
+});
+
+
 
 
 // Live Timer
